830b8a2576e57af07a058946b19b4f6b75d92150
Andrii Zhukovych <andriizhukovych@pingidentity.com>
2023-07-19 21:38:08 +0300

    UC#1 Create endpoint

--- src/App.js
+++ src/App.js
@@ -1,11 +1,70 @@
+import { useState, useEffect, useCallback } from "react";
 import './App.css';
 import axios from "axios";
 
 function App() {
+  const [countries, setCountries] = useState([]);
+  const [filter, setFilter] = useState({});
+  const getCountries = useCallback(() => {
+    axios.get('https://restcountries.com/v3.1/all')
+      .then(({ data }) => {
+        setCountries(data)
+      })
+      .catch((error) => console.log(error))
+  }, [])
+  useEffect(() => {
+    getCountries();
+  }, [])
+
+  const handleChange = (e, key) => {
+    setFilter({
+      ...filter,
+      [key]: e.target.value === '' ? null : e.target.value,
+    })
+  }
+
   return (
     <div className="App">
+      <div className="form">
+        <label>
+          Filter by name:
+          <input type="text" name="name" value={filter.name} onChange={(e) => handleChange(e, 'name')} />
+        </label>
+        <label>
+          Filter by population:
+          <input type="text" pattern="[0-9]*" name="population" onChange={(e) => handleChange(e, 'population')}/>
+        </label>
+        <label>
+          Sort:
+          <input type="text" name="sort" onChange={(e) => handleChange(e, 'sort')}/>
+        </label>
+        <label>
+          Pagination:
+          <input type="text" name="Pagination" />
+        </label>
+        <button>Submit</button>
+      </div>
+      <table className="table">
+        <thead>
+        <tr>
+          <th scope='col'>Name</th>
+          <th scope='col'>Population</th>
+        </tr>
+        </thead>
+        <tbody>
+        {countries.map(item => (
+          <tr>
+            <td>{item.name?.common} </td>
+            <td>{item.population} </td>
+          </tr>
+        ))}
+        </tbody>
+      </table>
     </div>
   );
 }
 
 export default App;
+
+
+
